daad68e363e6b5ca0cae79496ddcd9679d3aa0d4
Natalia Zhurbenko <natalie.zhurbenko@gmail.com>
2020-09-23 03:06:56 +0300

    Updates

---
+++ precache-manifest.18282a724629f42f7b5348af8534fe7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c398677869b215fc9d94b9335c2919",
+    "url": "/shopping-cart-demo/index.html"
+  },
+  {
+    "revision": "d1a36917064a3c24ddea",
+    "url": "/shopping-cart-demo/static/css/main.473abc24.chunk.css"
+  },
+  {
+    "revision": "b50df44a1f960381973b",
+    "url": "/shopping-cart-demo/static/js/2.643391a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart-demo/static/js/2.643391a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a36917064a3c24ddea",
+    "url": "/shopping-cart-demo/static/js/main.2488ff38.chunk.js"
+  },
+  {
+    "revision": "7453003e48fb69fe745f",
+    "url": "/shopping-cart-demo/static/js/runtime-main.5e7e4f2b.js"
+  }
+]);
